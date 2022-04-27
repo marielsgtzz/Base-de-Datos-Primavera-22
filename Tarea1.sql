d74fb0538e00df3a5aa4c883894fc5ee93f71425
Mariel Sofía Gutiérrez Zapién <marielsgtzz@MacBook-Pro-de-Mariel.local>
2022-04-27 11:46:25 -0500

    ej 9

--- Tarea1.sql
+++ Tarea1.sql
@@ -21,7 +21,18 @@ select o.order_id from orders o where not (o.ship_country = 'Brazil' or o.ship_c
 
 --Necesitamos los nombres completos de los empleados, nombres y apellidos unidos en un mismo registro
 
-
 --Cuánta lana tenemos en inventario?
+select SUM(p.units_in_stock * p.unit_price) as total_money_inventory from products p  ;
 
 --Cuantos clientes tenemos de cada país?
+
+--Obtener un reporte de edades de los empleados para checar su elegibilidad para seguro de gastos médicos menores.
+--Cuál es la orden más reciente por cliente?
+--De nuestros clientes, qué función desempeñan y cuántos son?
+--Cuántos productos tenemos de cada categoría?
+--Cómo podemos generar el reporte de reorder?
+--A donde va nuestro envío más voluminoso?
+--Cómo creamos una columna en customers que nos diga si un cliente es bueno, regular, o malo?
+--Qué colaboradores chambearon durante las fiestas de navidad?
+--Qué productos mandamos en navidad?
+--Qué país recibe el mayor volumen de producto?
