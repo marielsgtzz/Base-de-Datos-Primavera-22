122adf7a6d679b081232193b9e73adba5af08e1f
Mariel Sofía Gutiérrez Zapién <marielsgtzz@MacBook-Pro-de-Mariel.local>
2022-04-27 11:35:58 -0500

    ej 6

--- Tarea1.sql
+++ Tarea1.sql
@@ -13,10 +13,12 @@ select p.product_name from products p where p.category_id = 4;
 --Qué ordenes van a Bélgica o Francia?
 select o.order_id from orders o where o.ship_country = 'Belgium' or o.ship_country = 'France';
 --Qué órdenes van a LATAM?
+select o.order_id from orders o where o.ship_country = 'Brazil' or o.ship_country = 'Mexico' or o.ship_country = 'Venezuela' or o.ship_country = 'Argentina';
 
 --Qué órdenes no van a LATAM?
 
 --Necesitamos los nombres completos de los empleados, nombres y apellidos unidos en un mismo registro
+
 
 --Cuánta lana tenemos en inventario?
 
